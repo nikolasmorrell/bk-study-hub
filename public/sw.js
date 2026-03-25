15c527011dcbf2fd0d2a8a7ce2c67d7e86186f03
Nikolas Morrell <nikolasmorrell@gmail.com>
2026-03-24 20:51:59 -0400

    BK Study Hub - study planner & assignment timer PWA

---
+++ public/sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'studyhub-v1';
+const ASSETS = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+  ));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
+
+self.addEventListener('notificationclick', e => {
+  e.notification.close();
+  e.waitUntil(clients.openWindow('/'));
+});
